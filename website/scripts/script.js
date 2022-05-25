ba6f24b631f330de8d9c87575d8aaf1cc8de3978
Maciej Kaczorowski <maciejfkaczorowski@gmail.com>
2022-05-25 10:14:54 +0200

    first commit

---
+++ website/scripts/script.js
@@ -0,0 +1,120 @@
+function showAlertAndRemoveTile1() {
+  alert("Data removed!")
+
+  var tile = document.getElementById("tile-1")
+
+  if (tile.style = "display: block") {
+
+  tile.style = "display: none"}
+}
+
+
+function showAlertAndRemoveTile2() {
+  alert("Data removed!")
+
+  var tile = document.getElementById("tile-2")
+
+  if (tile.style = "display: block") {
+
+  tile.style = "display: none"}
+}
+
+function showAlertAndRemoveTile3() {
+  alert("Data removed!")
+
+  var tile = document.getElementById("tile-3")
+
+  if (tile.style = "display: block") {
+
+  tile.style = "display: none"}
+}
+
+function showAlertAndRemoveTile4() {
+   alert("Data removed!")
+
+   var tile = document.getElementById("tile-4")
+
+   if (tile.style = "display: block") {
+
+   tile.style = "display: none"}
+}
+
+function showAlertAndRemoveTile5() {
+  alert("Data removed!")
+
+  var tile = document.getElementById("tile-5")
+
+  if (tile.style = "display: block") {
+
+  tile.style = "display: none"}
+}
+
+function showAlertAndRemoveTile6() {
+  alert("Data removed!")
+
+  var tile = document.getElementById("tile-6")
+
+  if (tile.style = "display: block") {
+
+  tile.style = "display: none"}
+}
+
+function showAlertAndRemoveTile7() {
+  alert("Data removed!")
+
+  var tile = document.getElementById("tile-7")
+
+  if (tile.style = "display: block") {
+
+  tile.style = "display: none"}
+}
+
+function showAlertAndRemoveTile8() {
+  alert("Data removed!")
+
+  var tile = document.getElementById("tile-8")
+
+  if (tile.style = "display: block") {
+
+  tile.style = "display: none"}
+}
+
+function showAlertAndRemoveTile9() {
+  alert("Data removed!")
+
+  var tile = document.getElementById("tile-9")
+
+  if (tile.style = "display: block") {
+
+  tile.style = "display: none"}
+}
+
+function showAlertAndRemoveTile10() {
+  alert("Data removed!")
+
+  var tile = document.getElementById("tile-10")
+
+  if (tile.style = "display: block") {
+
+  tile.style = "display: none"}
+}
+
+function showAlertAndRemoveTile11() {
+  alert("Data removed!")
+
+  var tile = document.getElementById("tile-11")
+
+  if (tile.style = "display: block") {
+
+  tile.style = "display: none"}
+}
+
+function showAlertAndRemoveTile12() {
+  alert("Data removed!")
+
+  var tile = document.getElementById("tile-12")
+
+  if (tile.style = "display: block") {
+
+  tile.style = "display: none"}
+}
